cdffb8db797a62b18d09e021c00e254709bf9bf7
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-07-16 19:41:46 -0300

    criando a classe microfone

--- construir-objetos/class.js
+++ construir-objetos/class.js
@@ -14,3 +14,19 @@
   2 - O "new" NÃO cria uma instância da class, mas um objeto com cadeia de prototótipos;
 
 */
+
+class Microfone {
+  constructor(color = "black") {
+    this.color = color;
+    this.isOn = true;
+  }
+
+  toggleOnOff() {
+    if(microphone.isOn){
+      console.log("desligar");
+    } else {
+      console.log("ligar");
+    }
+    isOn = !isOn;
+  }
+}
--- construir-objetos/prototype.js
+++ construir-objetos/prototype.js
@@ -15,8 +15,22 @@
 
 function Microphone(color = "black") {
   this.color = color;
-} 
+
+  this.isOn = true;
+}
+
+// Microphone.prototype.toggleOnOff é o mesmo que colocar dentro de Microphone e chamar a função através de this.toggleOnOff
+
+Microphone.prototype.toggleOnOff = function () {
+  if(this.isOn){
+    console.log("desligar");
+  } else {
+    console.log("ligar");
+  }
+  this.isOn = !this.isOn;
+}
 
 const microphone = new Microphone();
+const microphone2 = new Microphone("white");
 
 console.log(microphone)
